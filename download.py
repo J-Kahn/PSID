e98cd00be72b4e57ce6a4e8f7ab04bec84583da8
SushiK3 <robert.kahn@simon.rochester.edu>
2014-10-18 11:02:55 -0400

    Working copy

--- download.py
+++ download.py
@@ -3,17 +3,30 @@ import lxml.html
 import os
 import os.path
 import zipfile
+import sas
+import gc
 
+# Keep track of starting directory for convenience
 start_dir       = os.getcwd()
-target_dir      = '/home/PSID'
-user            = ''
-password        = ''
-login_url       = 'http://simba.isr.umich.edu/u/Login.aspx'
 
+target_dir      = '/home/PSID' # Enter the directory you want the PSID files in
+user            = '' # Enter your user name, you must register at psidonline.isr.umich.edu
+password        = '' # Enter associated password
+login_url       = 'http://simba.isr.umich.edu/u/Login.aspx' # Logi url
+
+# Make target directory if it does not exist
+if not os.path.exists(target_dir):
+	
+	os.makedirs(target_dir)
+
+
+# Go to target directory
 os.chdir(target_dir)
 
+# Start request session and go to login URL
 session         = requests.session()
 
+# Get out VIEWSTATE and EVENTVALIDATION variables
 start           = session.get(login_url)
 html            = start.text
 root            = lxml.html.fromstring(html)
@@ -30,50 +43,96 @@ acc_pwd         = {	'ctl00$ContentPlaceHolder1$Login1$UserName'    : user,
 					'__EVENTVALIDATION'                           : EVENTVALIDATION
 				}
 
+# Send login message to PSID site
 session.post(login_url, data=acc_pwd)
 
+# Check for login
 z=session.get('http://simba.isr.umich.edu/data/data.aspx')
 tf2 = 'Logout' in z.content
 print 'Successful login: ' + str(tf2)
 
+# File years, numbers and labels
 
-file_year       = range(1968,1998) + range( 1999 , 2012, 2 )
-request_numbers = [1056 ] + range(1058,1082) + [1040, 1052 , 1132 , 1139 , 1152 , 1156]
+file_year       = range(1968,1998) + range( 1999 , 2012, 2 ) 
+request_numbers = [1056 ] + range(1058,1083) +  range(1047,1052) + [1040, 1052 , 1132 , 1139 , 1152 , 1156] 
 request_start   = 'http://simba.isr.umich.edu/Zips/GetFile.aspx?file='
-file_start      = 'dat'
+file_start      = 'FAM'
 
+# Function to download PSID zip file
 def download_psid(number, local_filename, sessions):
+
+	# Get the file using requests
 	r = sessions.get(request_start + number, stream=True)
+
 	with open(local_filename, 'wb') as f:
+
+		#Write it out in chunks incase it's big
 		for chunk in r.iter_content(chunk_size=1024):
-			if chunk: # filter out keep-alive new chunks
+
+			if chunk: 
 				f.write(chunk)
 				f.flush()
+
 	return local_filename
 
-def zip_unzip(filename):
-	zfile = zipfile.ZipFile(filename)
-	for name in zfile.namelist():
-		(dirname, filename) = os.path.split(name)
-		print "Decompressing " + filename + " on " + dirname
-		if not dirname == '':
-			if not os.path.exists(dirname):
-				os.makedirs(dirname)
-	zfile.extract(name, dirname)
-#try: 
-for i in range(0,len(request_numbers)):
-	print str(file_year[i])
-	file_name = file_start + str(file_year[i])
+# Extracting PSID using psid_unzip. Extractall will also extract STATA .do files, etc.
+def psid_unzip(filename, extractall = False):
 
+	zfile = zipfile.ZipFile(filename)
+
+	for name in zfile.namelist():
+
+		# Only take out the files we want
+		if '.sas' in name or '.txt' in name or '.pdf' in name or extractall == True:
+
+			(dirname, filename) = os.path.split(name)
+
+			if '.pdf' in name:
+				dirname = dirname + "Codebooks" # Different directory for Codebooks
+			if '.txt' in name:
+				nascii = name # Keep track of ascii name
+			if '.sas' in name:
+				nsas   = name # Keep track of sas name
+
+			print "Decompressing " + filename + " on " + dirname
+				
+			if not dirname == '':
+
+				if not os.path.exists(dirname):
+
+					os.makedirs(dirname)
+
+			zfile.extract(name, dirname) # Extract file
+
+	return (nsas, nascii)
+
+for i in range(0,len(request_numbers)):
+
+	print 'PSID year: ' + str(file_year[i])
+	file_name = file_start + str(file_year[i]) + '.zip'
+
+	# Grab file
 	x = download_psid(str(request_numbers[i]), file_name, session)
+	
 	print "Downloaded"
-	zip_unzip(file_name)
+	
+	# Unzip
+	nsas, nascii = psid_unzip(file_name)
+	
 	print "Unzipped"
 
+	# Turn it into a .csv
+	sas.sas_to_csv(nsas, nascii, file_start + str(file_year[i]) + '.csv')
+
+	print "Converted to CSV"
+
+	# Cleanup
+	os.remove(nsas)
+	os.remove(nascii)
 	os.remove(file_name)
+	
 	print "Deleted"
+	gc.collect()
+
 
 os.chdir(start_dir)
-
-#except:
-#	os.chdir(start_dir)
--- sas.py
+++ sas.py
@@ -1,29 +1,33 @@
 import re
 import numpy as np
 
-sas_to_csv(sas_name, ascii_name, csv_name)
+def sas_to_csv(sas_name, ascii_name, csv_name):
+
+	# Open sas file
 	x         = open(sas_name,"r")
 	dat       = x.read()
 	dat_split = dat.split('\n')
 
-	re_var    = '^\s*(?P<variable>\S+)\s+'
-	re_label  = '[(LABEL)(label)]\s*=\s*"(?P<label>[^"]+)"'
-	re_format = '[(FORMAT)(format)]\s*=\s*(?P<format>\S+)\s'
-	re_length = '\s*(?P<length1>\d*)\s+-\s+(?P<length2>\d*)\s*'
+	re_var    = '^\s*(?P<variable>\S+)\s+' # RE for variable designation
+	re_label  = '[(LABEL)(label)]\s*=\s*"(?P<label>[^"]+)"' # RE for variable label
+	re_format = '[(FORMAT)(format)]\s*=\s*(?P<format>\S+)\s' # RE for variable format
+	re_length = '\s*(?P<length1>\d*)\s*-\s*(?P<length2>\d*)\s*' # RE for variable position
 
 	meta = []
 
 	for dstr in dat_split:
-		res_var    = re.search(re_var, dstr)
-		res_label  = re.search(re_label, dstr)
-		res_format = re.search(re_format, dstr)
+		res_var    = re.search(re_var, dstr) # Find variable name in line
+		res_label  = re.search(re_label, dstr) # Find variable label
+		res_format = re.search(re_format, dstr) # Find variable format
 
 		if not (res_var is None or res_label is None or res_format is None):
 
-			counts = re.search(res_var.group('variable')+re_length,dat)
-			l1     = int(counts.group('length1'))
-			l2     = int(counts.group('length2'))
+			# Now that we have a verified variable name...
+			counts = re.search(res_var.group('variable')+re_length,dat) # Find position RE
+			l1     = int(counts.group('length1')) # Grab out first position
+			l2     = int(counts.group('length2')) # Grab out second position
 
+			# Add to meta data
 			meta        = meta +[ {	'variable'    : res_var.group('variable'),
 								'label'    : res_label.group('label'),
 								'format' : res_format.group('format'),
@@ -32,9 +36,13 @@ sas_to_csv(sas_name, ascii_name, csv_name)
 								'l3' : l2 - l1 + 1
 							}]
 
+	# Get relevant descriptions
 	names   = [z['label'] for z in meta]
 	formats = [z['format'] for z in meta]
 	lengths = [z['l3'] for z in meta]
+	del meta
 
+	# Use numpy to read fixed width file and write as .csv
 	data = np.genfromtxt(ascii_name, names=names, delimiter=lengths)
 	np.savetxt(csv_name, data, delimiter=',', header=','.join(data.dtype.names))
+	del data
